7cda1633ff1072ce0e7b9563cd754fe9f15f97b7
pangolin0 <kartikayerishi@gmail.com>
2021-01-01 16:12:11 +0530

    initial commit

---
+++ isoburn.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+echo "starting to burn your iso. Warning all data on the drive will be deleted. press Ctrl+C to quit in the next5 seconds."
+sleep 5
+wodim -eject -tao speed=2 dev=$1 -v -data $2
+echo "complete."
